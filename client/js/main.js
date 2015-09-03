cd4506bd21f3bf3b17396b6648b9a08de92acfcf
0x6C77 <flabbyrabbit@gmail.com>
2015-09-03 12:16:28 +0100

    Another fix for client ratings

--- client/js/main.js
+++ client/js/main.js
@@ -106,12 +106,12 @@ $(function() {
         if (data.rating > 1) {
             $('#controls .control--dislike .count').text(parseInt($('#controls .control--dislike .count').text()) - 1);
             if (parseInt($('#controls .control--dislike .count').text()) < 1) {
-                $('#controls .control--dislike .count').text().hide();
+                $('#controls .control--dislike .count').hide();
             }
         } else if (data.rating < -1) {
             $('#controls .control--like .count').text(parseInt($('#controls .control--like .count').text()) - 1).show();
             if (parseInt($('#controls .control--like .count').text()) < 1) {
-                $('#controls .control--like .count').text().hide();
+                $('#controls .control--like .count').hide();
             }
         }
     });
